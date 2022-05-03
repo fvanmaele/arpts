da7cb369b31c8c8400bd0af026d0fe7b514a9ea3
Ferdinand Vanmaele <Vanmaele@stud.uni-heidelberg.de>
2022-05-03 20:33:17 +0200

    generate matrices for N=512 and N=2048

--- matlab/main.m
+++ matlab/main.m
@@ -1,10 +1,12 @@
 %% Write matrices to Matrix Market (MM) files
-N = 2048;
 rng('default')  % default seed (0) and algorithm (Mersenne Twister)
 
-for Id = 1:20
-    fprintf("Generating matrix %02d\n", Id)
-    fname = sprintf("%02d-%d.mtx", Id, N);
-    A = generate_matrix(Id, N);
-    mmwrite(fname, A)
+for N = [512 2048]
+    for Id = 1:20
+        fprintf("Generating matrix %02d, N = %d\n", Id, N)
+        fname = sprintf("%02d-%d.mtx", Id, N);
+
+        A = generate_matrix(Id, N);
+        mmwrite(fname, A)
+    end
 end
