1bda16c1914ec5dca4e5994495f3f267575a95de
Ferdinand Vanmaele <Vanmaele@stud.uni-heidelberg.de>
2022-05-14 16:26:26 +0200

    increase number of samples for randomly generated partitions

--- python/distribution_run.sh
+++ python/distribution_run.sh
@@ -2,7 +2,17 @@
 set -e
 M=32 N=512
 
+cd sol_unifdist
 for id in $(seq 1 20); do
     echo "Computing distribution for matrix $id"
-    ./distribution.py "$id" "$N" "$M" --seed 0 --n-trials 5000 --distribution uniform
+    ../distribution.py "$id" "$N" "$M" --seed 0 \
+        --rand-n-samples 10000 --n-trials 5000 --distribution 'uniform'
+done
+cd -
+
+cd sol_normdist
+for id in $(seq 1 20); do
+    echo "Computing distribution for matrix $id"
+    ../distribution.py "$id" "$N" "$M" --seed 0 \
+        --rand-n-samples 10000 --n-trials 5000 --distribution 'normal'
 done
