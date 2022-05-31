b61a6dea5eab24c6bbd53de85dfc44773c1a490c
Ferdinand Vanmaele <Vanmaele@stud.uni-heidelberg.de>
2022-05-31 14:18:28 +0200

    update cumulative distribution

--- python/distribution_run.sh
+++ python/distribution_run.sh
@@ -1,31 +1,13 @@
 #!/bin/bash
 set -e
-M=32 N=512 n_trials=10000
+M=32 N=512 n_trials=1000
+distribution=${1:-normal}
+distribution_setup=${2:-normal}
 
-for id in $(seq 1 20); do
-    echo "Computing distribution for matrix $id, solutions ~ normal, generated from ~ normal"
+for id in $(seq 1 25); do
+    printf 'Computing distribution for matrix %d, solutions ~ %s, generated from ~ %s' "$id" "$distribution" "$distribution_setup"
     ./distribution.py "$id" "$N" "$M" --seed 0 \
-        --rand-n-samples 5000 --n-trials "$n_trials" --distribution normal --distribution-setup normal
+                      --rand-n-samples 5000 --n-trials "$n_trials" \
+                      --distribution "$distribution" \
+                      --distribution-setup "$distribution_setup"
 done
-#mv ecdf*.png part_normdist/sol_normdist
-
-for id in $(seq 1 20); do
-    echo "Computing distribution for matrix $id, solutions ~ uniform, generated from ~ normal"
-    ./distribution.py "$id" "$N" "$M" --seed 0 \
-        --rand-n-samples 5000 --n-trials "$n_trials" --distribution uniform --distribution-setup normal
-done
-#mv ecdf*.png part_normdist/sol_unifdist
-
-for id in $(seq 1 20); do
-    echo "Computing distribution for matrix $id, solutions ~ normal, generated from ~ uniform"
-    ./distribution.py "$id" "$N" "$M" --seed 0 \
-        --rand-n-samples 5000 --n-trials "$n_trials" --distribution normal --distribution-setup uniform
-done
-#mv ecdf*.png part_unifdist/sol_normdist
-
-for id in $(seq 1 20); do
-    echo "Computing distribution for matrix $id, solutions ~ uniform, generated from ~ uniform"
-    ./distribution.py "$id" "$N" "$M" --seed 0 \
-        --rand-n-samples 5000 --n-trials "$n_trials" --distribution uniform --distribution-setup uniform
-done
-#mv ecdf*.png part_unifdist/sol_unifdist
