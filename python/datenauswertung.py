d62191448089f45a19d4347d6c9e443a1619678f
Ferdinand Vanmaele <Vanmaele@stud.uni-heidelberg.de>
2022-08-26 02:17:04 +0200

    save both relres and fre

--- python/datenauswertung.py
+++ python/datenauswertung.py
@@ -15,20 +15,20 @@ trials = [
     # 'trials_11-512-1e+05_512_sol-norm_gen-norm.json',
     # 'trials_11-512-1e+06_512_sol-norm_gen-norm.json',
     # 'trials_11-512-1e+07_512_sol-norm_gen-norm.json',
-    'trials_11-512-1e+08_512_sol-norm_gen-norm.json',
-    'trials_11-512-1e+09_512_sol-norm_gen-norm.json',
-    'trials_11-512-1e+10_512_sol-norm_gen-norm.json',
-    'trials_11-512-1e+11_512_sol-norm_gen-norm.json',
+    'trials_res_11-512-1e+09_512_sol-norm_gen-norm.json',
+    'trials_res_11-512-1e+10_512_sol-norm_gen-norm.json',
+    'trials_res_11-512-1e+08_512_sol-norm_gen-norm.json',
+    'trials_res_11-512-1e+11_512_sol-norm_gen-norm.json',
     # 'trials_11-512-1e+12_512_sol-norm_gen-norm.json',
     # 'trials_14-512-0e+00_512_sol-norm_gen-norm.json',
     # 'trials_14-512-1e-01_512_sol-norm_gen-norm.json',
     # 'trials_14-512-1e-02_512_sol-norm_gen-norm.json',
     # 'trials_14-512-1e-03_512_sol-norm_gen-norm.json',
     # 'trials_14-512-1e-04_512_sol-norm_gen-norm.json',
-    'trials_14-512-1e-05_512_sol-norm_gen-norm.json',
-    'trials_14-512-1e-06_512_sol-norm_gen-norm.json',
-    'trials_14-512-1e-07_512_sol-norm_gen-norm.json',
-    'trials_14-512-1e-08_512_sol-norm_gen-norm.json'
+    'trials_res_14-512-1e-05_512_sol-norm_gen-norm.json',
+    'trials_res_14-512-1e-06_512_sol-norm_gen-norm.json',
+    'trials_res_14-512-1e-07_512_sol-norm_gen-norm.json',
+    'trials_res_14-512-1e-08_512_sol-norm_gen-norm.json'
 ]
 
 # %%
@@ -50,7 +50,8 @@ foo = {
        'rand_min_cond_unif': 'II',
        'reduce_min_cond': 'III',
        'static_M32': 'IV',
-       'static_min_cond': 'V'
+       'static_min_cond': 'V',
+       'static_min_res': 'VI'
        }
 
 # %%
@@ -68,28 +69,28 @@ for T in sorted(trials_dict.keys()):
     images.append([x, y, e])
     
 # %%
-fig, axs = plt.subplots(4, 2)
+fig, axs = plt.subplots(4, 2, sharex=True)
 axs[0, 0].errorbar(images[0][0], images[0][1], images[0][2], linestyle='None', marker='o')
 axs[0, 0].set_title('11-512-1e+08')
-axs[0, 1].errorbar(images[1][0], images[1][1], images[1][2], linestyle='None', marker='o')
-axs[0, 1].set_title('11-512-1e+09')
-axs[1, 0].errorbar(images[2][0], images[2][1], images[2][2], linestyle='None', marker='o')
-axs[1, 0].set_title('11-512-1e+10')
-axs[1, 1].errorbar(images[3][0], images[3][1], images[3][2], linestyle='None', marker='o')
-axs[1, 1].set_title('11-512-1e+11')
+axs[1, 0].errorbar(images[1][0], images[1][1], images[1][2], linestyle='None', marker='o')
+axs[1, 0].set_title('11-512-1e+09')
+axs[2, 0].errorbar(images[2][0], images[2][1], images[2][2], linestyle='None', marker='o')
+axs[2, 0].set_title('11-512-1e+10')
+axs[3, 0].errorbar(images[3][0], images[3][1], images[3][2], linestyle='None', marker='o')
+axs[3, 0].set_title('11-512-1e+11')
 
-axs[2, 0].errorbar(images[4][0], images[4][1], images[4][2], linestyle='None', marker='o')
-axs[2, 0].set_title('14-512-1e-05')
-axs[2, 1].errorbar(images[5][0], images[5][1], images[5][2], linestyle='None', marker='o')
-axs[2, 1].set_title('14-512-1e-06')
-axs[3, 0].errorbar(images[6][0], images[6][1], images[6][2], linestyle='None', marker='o')
-axs[3, 0].set_title('14-512-1e-07')
+axs[0, 1].errorbar(images[4][0], images[4][1], images[4][2], linestyle='None', marker='o')
+axs[0, 1].set_title('14-512-1e-05')
+axs[1, 1].errorbar(images[5][0], images[5][1], images[5][2], linestyle='None', marker='o')
+axs[1, 1].set_title('14-512-1e-06')
+axs[2, 1].errorbar(images[6][0], images[6][1], images[6][2], linestyle='None', marker='o')
+axs[2, 1].set_title('14-512-1e-07')
 axs[3, 1].errorbar(images[7][0], images[7][1], images[7][2], linestyle='None', marker='o')
 axs[3, 1].set_title('14-512-1e-08')
 
-for ax in axs.flat:
-    ax.set(xlabel='Case', ylabel='FRE')
+# for ax in axs.flat:
+#     ax.set(xlabel='Case', ylabel='relres')
 
 # Hide x labels and tick labels for top plots and y ticks for right plots.
-for ax in axs.flat:
-    ax.label_outer()
+# for ax in axs.flat:
+#     ax.label_outer()
--- python/distribution_run.sh
+++ python/distribution_run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-M=32 N=512 n_trials=2500
+M=32 N=512 n_trials=500
 distribution=${1:-normal}
 distribution_setup=${2:-normal}
 
@@ -26,7 +26,7 @@ mtx=('11-512-1e+04'
 for id in "${mtx[@]}"; do
     printf 'Computing distribution for matrix %s, solutions ~ %s, generated from ~ %s\n' "$id" "$distribution" "$distribution_setup"
     python distribution.py "$id" "$N" "$M" --seed 0 --n-trials "$n_trials" \
-                      --rand-n-samples 10000 --rand-min-part 24 --rand-max-part 48 --rand-mean 32 --rand-sd 2 \
+                      --rand-n-samples 2000 --rand-min-part 24 --rand-max-part 48 --rand-mean 32 --rand-sd 2 \
                       --static-M-min 16 --static-M-max 64 --static-min-part 16 \
                       --cond-lo-min 16 --cond-lo-max 48 --cond-hi-min 32 --cond-hi-max 64 --cond-min-part 16 \
                       --distribution "$distribution" \
